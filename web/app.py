b7558abe9d0f513cc06700133bc4251c262eaf2f
S. Guliaev <dev.sagul@gmail.com>
2020-05-09 16:35:47 +0300

    add new polygons for mock

--- web/app.py
+++ web/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def polygons():
 
     res = [
-          {
+        {
             "name": "United States",
             "coordinates": [
             {
@@ -25,6 +25,50 @@ def polygons():
             }
             ],
             "type": "red"
+        },
+                {
+            "name": "United States",
+            "coordinates": [
+            {
+                "lat": 55.849678,
+                "lon": 37.510379
+            },
+            {
+                "lat": 55.836667,
+                "lon": 37.675627
+            },
+            {
+                "lat": 55.703241,
+                "lon": 37.639402
+            }
+            ],
+            "type": "red"
+        },
+                {
+            "name": "United States",
+            "coordinates": [
+            {
+                "lat": 55.819678,
+                "lon": 37.510379
+            },
+            {
+                "lat": 55.826667,
+                "lon": 37.625627
+            },
+            {
+                "lat": 55.733241,
+                "lon": 37.639402
+            },
+                        {
+                "lat": 55.743241,
+                "lon": 37.649402
+            },
+                        {
+                "lat": 55.753241,
+                "lon": 37.659402
+            }
+            ],
+            "type": "green"
         },
     ]
 
