1ad69ab38a4cf8586844f3ab4fbcf466c6a3eeae
S. Guliaev <dev.sagul@gmail.com>
2020-05-09 22:25:50 +0300

    fix reports api

--- web/app.py
+++ web/app.py
@@ -1,3 +1,4 @@
+import datetime
 from flask import Flask, jsonify
 from flask_cors import CORS, cross_origin
 
@@ -76,74 +77,15 @@ def upload():
 @cross_origin
 @app.route('/api/v1/reports')
 def get_reports():
-    polygon1 = {
-        "id": "Кадастровый номер1",
-        "name": "United States",
-        "coordinates": [
-        {
-            "lat": 55.80166847150037,
-            "lon": 37.75216549902341
-        },
-        {
-            "lat": 55.80862947567413,
-            "lon": 37.87713498144529
-        },
-        {
-            "lat": 55.731989658541536,
-            "lon": 37.92382687597654
-        },
-        {
-            "lat": 55.70951094778051,
-            "lon": 37.74529904394527
-        }
-        ],
-        "type": "red"
-    }
-
     report1 = {
-        "area": polygon1,
-        "coords": [55.80862947567413, 37.87713498144529],
-        "sqaure": 4,
-        "areaPurpose": "Тип использования",
-        "type": "Тип участка",
-        "district": "Кадастровый номер квартала",
+        "date": datetime.datetime.today(),
+        "name": "United States",
         "id": "Кадастровый номер1",
-    }
-    polygon2 = {
-        "id": "Кадастровый номер2",
-        "name": "Green",
-        "coordinates": [
-        {
-            "lat": 55.77613409402249,
-            "lon": 37.34979123144528
-        },
-        {
-            "lat": 55.7784560956789,
-            "lon": 37.411589327148405
-        },
-        {
-            "lat": 55.725789932653754,
-            "lon": 37.441801729492155
-        },
-        {
-            "lat": 55.6955521084172,
-            "lon": 37.35391110449215
-        },
-        {
-            "lat": 55.75213194030574,
-            "lon": 37.23718136816404
-        }
-        ],
-        "type": "green"
     }
 
     report2 = {
-        "area": polygon2,
-        "coords": [55.75213194030574, 37.23718136816404],
-        "sqaure": 10,
-        "areaPurpose": "Тип использования",
-        "type": "Тип участка",
-        "district": "Кадастровый номер квартала",
+        "date": datetime.datetime.today(),
+        "name": "Green",
         "id": "Кадастровый номер2",
     }
     res = [
@@ -182,7 +124,7 @@ def get_report(id):
     res = {
         "area": polygon,
         "coords": [55.80862947567413, 37.87713498144529],
-        "sqaure": 4,
+        "square": 4,
         "areaPurpose": "Тип использования",
         "type": "Тип участка",
         "district": "Кадастровый номер квартала",
