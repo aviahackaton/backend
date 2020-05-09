1697c8d031427eb824741be9106da782786b3de4
S. Guliaev <dev.sagul@gmail.com>
2020-05-10 00:17:13 +0300

    ad detatime to report api

--- web/app.py
+++ web/app.py
@@ -100,6 +100,7 @@ def get_reports():
 def get_report(id):
     polygon = {
         "id": 1,
+        "date": int(time.time()),
         "name": "United States",
         "coordinates": [
         {
